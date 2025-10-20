3ed2f05ebbc16de14c71fbdf1e4299128fa85371
theodoraaaa <theacommandeur@icloud.com>
2025-10-20 13:03:21 +0100

    Starting to look at data types

---
+++ module2/datatypes/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Name of user
+string
+// Age of user
+int
+// Whether they have opted in to the newsletter
+bool
+
+int // used to store whole numbers
+
+char // used to store a single character
+
+bool // used to store true/false values
+
+double // used to store a decimal value
+
+string // used to store textual information

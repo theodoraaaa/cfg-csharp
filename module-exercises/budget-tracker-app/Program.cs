d611f64260b352701b81a2dded3993663e036d00
theodoraaaa <theacommandeur@icloud.com>
2025-10-23 20:06:56 +0100

    first version of budget tracker

--- module-exercises/budget-tracker-app/Program.cs
+++ module-exercises/budget-tracker-app/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to our budget tracker");
+Console.WriteLine("Welcome to our budget tracker app!");
+
+// 1. Create a variable called groceryBudget and set a monthly budget
+double groceryBudget = 200.00;
+
+// 2. Create a variable called entertainmentBudget and set a monthly budget
+double entertainmentBudget = 250.00;
+
+// 3. Use Console.WriteLine() to output these variables to the console
+Console.WriteLine($"Grocery budget: {groceryBudget}");
+Console.WriteLine($"Entertainment budget: {entertainmentBudget}");
+
+// 4. Create an array called fixedExpenses to store 3 double variables (one for rent, one for electricity and one for water)
+// double[] fixedExpenses = { 1200.00, 150.00, 50.00 };
+
+//    and output the contents of the array to the console
+// Console.WriteLine("Fixed Expenses: ");
+// Console.WriteLine("Rent: " + fixedExpenses[0]);
+// Console.WriteLine("Electricity: " + fixedExpenses[1]);
+// Console.WriteLine("Water: " + fixedExpenses[2]);
+
+// 5. Replace the array with a List
+List<double> fixedExpenses = new List<double> { 1200.00, 150.00, 50.00 };
+Console.WriteLine("Fixed Expenses: ");
+Console.WriteLine("Rent: " + fixedExpenses[0]);
+Console.WriteLine("Electricity: " + fixedExpenses[1]);
+Console.WriteLine("Water: " + fixedExpenses[2]);
+
+// 6. Add some new expenses to the List using List methods
+fixedExpenses.Add(35.00); // Internet
+fixedExpenses.Add(9.99);  // Spotify
+
+Console.WriteLine("Internet: " + fixedExpenses[3]);
+Console.WriteLine("Spotify: " + fixedExpenses[4]);
--- module-exercises/budget-tracker-app/obj/Debug/net9.0/budget-tracker-app.AssemblyInfo.cs
+++ module-exercises/budget-tracker-app/obj/Debug/net9.0/budget-tracker-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("budget-tracker-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1fe722ea98c7f6e5595061b99839fa63c0f3f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88368529f070295d6c0b94c0918527cd59115634")]
 [assembly: System.Reflection.AssemblyProductAttribute("budget-tracker-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("budget-tracker-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

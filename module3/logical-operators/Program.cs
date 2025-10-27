569735b00e539248e281799e98867049dc417ee7
theodoraaaa <theacommandeur@icloud.com>
2025-10-27 16:02:06 +0000

    running through if else statements with logical operators

---
+++ module3/logical-operators/Program.cs
@@ -0,0 +1,28 @@
+﻿// smoothie example
+
+bool strawberry = true;
+bool banana = false;
+
+if (strawberry && banana)
+{
+    Console.WriteLine("You can make a smoothie!");
+}
+else if (strawberry || banana)
+{
+    Console.WriteLine("You can eat a piece of fruit.");
+}
+else
+{
+    Console.WriteLine("You need more ingredients to make a smoothie.");
+}
+
+// raining example
+
+bool raining = true;
+
+if (!raining) {
+    Console.WriteLine("It's not raining outside.");
+}
+else {
+    Console.WriteLine("It's raining outside. Take an umbrella!");
+}

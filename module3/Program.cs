aedf59ace196c60c9eadd0de9a5823b034777c1f
theodoraaaa <theacommandeur@icloud.com>
2025-10-27 15:08:08 +0000

    comparison operators and how to use them

---
+++ module3/Program.cs
@@ -0,0 +1,25 @@
+﻿// ==
+
+string firstString = "Hello";
+string secondString = "CFG";
+
+Console.WriteLine(firstString == secondString);
+
+// > <
+
+int firstNumber = 10;
+int secondNumber = 5;
+
+Console.WriteLine(firstNumber > secondNumber);
+
+// >= <=
+
+Console.WriteLine(firstNumber >= secondNumber);
+
+int thirdNumber = 10;
+
+Console.WriteLine(firstNumber <= thirdNumber);
+
+// !=
+
+Console.WriteLine(firstString != secondString);

660c3978e69ef9dad4d55062b5efb3a9b6a75000
theodoraaaa <theacommandeur@icloud.com>
2025-10-27 18:08:59 +0000

    practicing while loops

---
+++ module4/while-loops/Program.cs
@@ -0,0 +1,12 @@
+﻿// have to initialise i before the loop
+// need the condition in the while statement
+// need to increment i inside the loop body
+// need to make sure the while statement will eventually be false
+
+int i = 1;
+
+while (i <= 10)
+{
+    Console.WriteLine(i);
+    i++;
+}

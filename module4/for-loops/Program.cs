bb63422fba9d81c3669e62c5fb4d767d96f56b33
theodoraaaa <theacommandeur@icloud.com>
2025-10-27 17:51:53 +0000

    for loop syntax

---
+++ module4/for-loops/Program.cs
@@ -0,0 +1,9 @@
+﻿// three things we need
+// 1. initialiser - do something to this every loop
+// 2. condition - for loop runs while this is true
+// 3. modifier - what happens to i each loop
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}

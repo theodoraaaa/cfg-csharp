d5068a87093fbdcbd645438b0e65336359f0c460
theodoraaaa <theacommandeur@icloud.com>
2025-10-30 16:03:15 +0000

    learning about functions and methods

---
+++ module4/methods/Program.cs
@@ -0,0 +1,15 @@
+﻿// start with the return type of the method
+
+int AddNumbers (int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+
+AddNumbers(10, 5);
+Console.WriteLine(AddNumbers(10, 5));
+
+// or
+
+int sum = AddNumbers(10, 5);
+Console.WriteLine("The sum of these two numbers is " + sum);

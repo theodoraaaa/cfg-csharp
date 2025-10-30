f54725d0a2de21e28b4c4827db7512cf58fdf4a8
theodoraaaa <theacommandeur@icloud.com>
2025-10-30 16:22:08 +0000

    using the car class

---
+++ module4/oop/Program.cs
@@ -0,0 +1,9 @@
+﻿Car myCar = new Car();
+
+myCar.Model = "Yaris";
+myCar.Make = "Toyota";
+myCar.Colour = "Blue";
+
+myCar.Drive();
+
+Console.WriteLine($"My car is a {myCar.Colour} {myCar.Make} {myCar.Model}.");
